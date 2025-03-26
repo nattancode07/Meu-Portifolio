c5b249187682eb806e4af87c3b4afe184c2a239f
Elyaquim Nattan <elyaquimnattan@gmail.com>
2025-03-26 17:19:48 -0300

    commit about section

---
+++ app/components/Header.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Image from 'next/image'
+import { assets } from '@/assets/assets'
+const Header = () => {
+  return (
+    <div className='w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4'>
+      <div>
+        <Image src={assets.profile_img} alt='' className='rounded-full w-32'/>
+      </div>
+
+      <h3 className='flex items-end gap-2 text-x1 md:text-2x1 mb-3 font-Ovo'>
+        Hi! I'm Elyaquim Nattan <Image src={assets.hand_icon} alt=''
+        className='w-6'/></h3>
+      <h1 className='text-3xl  sm:text-6x1 lg:text-[50px] font-Ovo'>
+        fullstack developer based in Brasil.
+      </h1>
+      <p className='max-w-2x1 mx-auto font-Ovo'>
+          I am a fullstack developer from Pernambuco, BR with 1 year of experience in some companies.
+      </p>
+      <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
+        <a href="#contact"
+        className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2'
+        >contact me <Image src={assets.right_arrow_white} alt='' className=' w-4'/></a>
+
+        <a href="/sample-resume.pdf" download 
+        className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2'>
+          my resume <Image src={assets.download_icon} alt='' className=' w-4'/></a>
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        lightHover: '#fcf4ff',
+        darkHover: '#2a004a',
+        darkTheme: '#11001F'
+      },
+      fontFamily: {
+        Outfit: ["Outfit", "sans-serif"],
+        Ovo: ["Ovo", "serif"]
+      },
+      boxShadow: {
+        'black' : '4px 4px 0px #000',
+        'white' : '4px 4px 0px #fff',
+      }
+    },
+  },
+  plugins: [],
+}
+
