a0f802eefdc0eefb63213c25e86cde0724ce93ac
Elyaquim Nattan <elyaquimnattan@gmail.com>
2025-04-15 17:40:47 -0300

    adicionando footer e parte do darkmode

--- app/page.js
+++ app/page.js
@@ -6,16 +6,41 @@ import About from './components/About';
 import Services from './components/Services';
 import Work from './components/Work';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
+import { useState, useEffect } from 'react';
 
 export default function Home() {
+
+  const [isDarkMode, setDarkMode] = useState(false);
+
+  useEffect(()=>{
+    if(localStorage.theme === 'dark' || (!('theme' in localStorage) && window.
+      matchMedia('(prefers-color-scheme: dark)').matches)){
+        setDarkMode(true)
+    }else{
+        setDarkMode(false)
+    }
+  },[])
+
+  useEffect(()=>{
+    if(isDarkMode){
+      document.documentElement.classList.add('dark');
+      localStorage.theme = 'dark';
+    }else{
+      document.documentElement.classList.remove('dark');
+      localStorage.theme = '';
+    }
+  }, [isDarkMode])
+
   return (
     <>
-    <Navbar/>
-    <Header/>
-    <About/>
-    <Services/>
-    <Work/>
-    <Contact/>
+    <Navbar isDarkMode={isDarkMode} setDarkMode={setDarkMode}/>
+    <Header isDarkMode={isDarkMode} setDarkMode={setDarkMode}/>
+    <About isDarkMode={isDarkMode} setDarkMode={setDarkMode}/>
+    <Services isDarkMode={isDarkMode} setDarkMode={setDarkMode}/>
+    <Work isDarkMode={isDarkMode} setDarkMode={setDarkMode}/>
+    <Contact isDarkMode={isDarkMode} setDarkMode={setDarkMode}/>
+    <Footer isDarkMode={isDarkMode} setDarkMode={setDarkMode}/>
     </>
   );
 }
