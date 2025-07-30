19f88a4bd1cce5d39f52b4eccb737223d1dd0111
Elyaquim Nattan <elyaquimnattan@gmail.com>
2025-07-30 10:20:36 -0300

    atualizando curriculo

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -48,7 +48,7 @@ const Header = () => {
         initial={{y: 30, opacity: 0}}
         whileInView={{y: 0, opacity: 1}}
         transition={{duration: 0.6, delay: 1.2}}
-        href="/CurriculoElyaquimNattan2025.pdf" download 
+        href="/Curriculo-ElyaquimNattan.pdf" download 
         className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black'>
           Meu curriculo <Image src={assets.download_icon} alt='' className=' w-4'/></motion.a>
       </div>
